ef9c839c2e5ac0dc7dc5f065a80aade9597a0990
Pedro <work.pedrocampelo@gmail.com>
2023-03-02 10:12:38 +0100

    Update Cell.java

--- src/main/java/org/example/Cell.java
+++ src/main/java/org/example/Cell.java
@@ -2,16 +2,25 @@ package org.example;
 
 public class Cell {
 
-     boolean withMine;
-    int state;//1:covered, 2:uncovered, 3:marked
-    int column;
-    int row;
-
+    public Cell(boolean withMine, int state, int column, int row) {
+        this.withMine = withMine;
+        this.state = state;
+        this.column = column;
+        this.row = row;
+    }
     public Cell(int column, int row) {
         this.column = column;
         this.row = row;
 
     }
+
+
+    boolean withMine;
+    int state;//1:covered, 2:uncovered, 3:marked
+    int column;
+    int row;
+
+
 
 
 
